9c3f7b2483edb5b36aaa54b19f0b542afcbde909
AlexChi <alexchi1114@gmail.com>
2017-01-10 15:17:18 -0500

    Finished project. Added grayscale option. There is still some bugs associated with the fadein button

--- scripts/main.js
+++ scripts/main.js
@@ -4,37 +4,55 @@ $(document).ready(function(){
 
 	$("#erase").click(function(){
 		$(".box").css("background-color", "white");
-	});
-
-	$("#changesize").click(function(){
-		var dimension=prompt('What would you like the height and width of the grid to be?');
-		newGrid(dimension);
-	});
-
-	
-		$(document).on("mouseenter", ".box", function(){
-
-			$(this).css("background-color", "red");
-	});
-
-	$("#rainbow").click(function(){
-		confirm("Rainbow Mode Activated!");
-		var dimension=prompt('What would you like the height and width of the grid to be?');
-		newGrid(dimension);
-		$(document).on("mouseenter", ".box", function(){
-			$(this).css("background-color", "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")");
-		});
 		
 	});
+
+	$("#changesize").on("click", function(){
+		var dimension=prompt('What would you like the height and width of the grid to be? Your number must be an integer between 0 and 100');
+		if(dimension<1 || dimension>100){
+			confirm("That number is not an integer between 1 and 100!");
+			dimension=prompt('What would you like the height and width of the grid to be? Your number must be an integer between 0 and 100');
+		}
+		newGrid(dimension);
+	});
+
 	
-	
-Math.floor(Math.random()*255)
-	
+		$(document).on("mouseenter", ".box", function(){
+			
+			$(this).css("background-color", "black");
+
+
+	});
+
+	$("#rainbow").on("click", function(){
+		confirm("Rainbow Mode Activated!");
+		$(document).on("mouseenter", ".box", function(){
+			
+			$(this).css("background-color", "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")");
+		});	
+	});
+
+	$("#fadein").on("click", function(){
+		confirm("Fade In Mode Activated!");
+		$(".box").css("background-color", "white");
+		$(".box").css("opacity", 0);
+		
+		$(document).on("mouseenter", ".box", function(){
+		$(this).css("background-color", "black");
+
+			var opacity = +$(this).css('opacity');
+    		if (opacity < 1) {
+    	    opacity += 0.1;
+    	    $(this).css('opacity', opacity);
+    	}
+		});
+			
+	});
 });
 		
 var newGrid=function(number){
 	$("#grid").empty();
-	var boxsize=(432-2*number)/number;
+	var boxsize=400/number;
 	for(j=0;j<number*number;j++){
 			$("#grid").append('<div class="box"></div>');	
 	}
@@ -42,13 +60,7 @@ var newGrid=function(number){
 	$(".box").width(boxsize+'px');
 }
 
-var rainbow = function(){
-		$(".box").on("mouseenter", function(){
-			$(this).css("background-color", "green");
-			$(this).css("opacity","1.2");
-		return true;
-		});
-}
+
 
 
 	
